1b519b137e5dafcb27cee61c9900211a7b20da16
taiwoladipupo <taiwoladipupo@gmail.com>
2022-02-05 21:02:16 -0600

    LongestSubstringWithKDistinctCharacters

--- LongestSubStringWithMaximumKDistinctChars.java
+++ LongestSubStringWithMaximumKDistinctChars.java
@@ -3,7 +3,7 @@ package InterviewQuestions.SlidingWindow;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LongeestSubStringWithMaximumKDinstinctChars {
+public class LongestSubStringWithMaximumKDistinctChars {
     /*
         Difficulty : Medium
         This problem asked to find the Longest SubString with Maximum K unique characters
@@ -26,35 +26,35 @@ public class LongeestSubStringWithMaximumKDinstinctChars {
                                         Time Complexity = O(n)
                                         Space Complexity = O(1)
      */
-    public static int findMaxLength(String s, int k){
+    public static int findMaxLength(String s, int k) {
         //create variables windowStart, maxLength
         int windowStart = 0, maxLength = Integer.MIN_VALUE;
         //implement a HashMap to keep track of the frequency
         Map<Character, Integer> map = new HashMap<>();
         //traversing through the string
-        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++){
+        for (int windowEnd = 0; windowEnd < s.length(); windowEnd++) {
             char rightChar = s.charAt(windowEnd);
-            map.put(rightChar, map.getOrDefault(rightChar,0) + 1);
+            map.put(rightChar, map.getOrDefault(rightChar, 0) + 1);
             //the condition is to have a substring with <= K distinct characters
             //so since the characters are stored in the map
             //at any point where the size of the Map does not satisfy this condition i.e
             //the size of the map > K distinct chars
             //then we will begin to shrink the window until we are left with K distinct elements
-            while (map.size() > k){
+            while (map.size() > k) {
                 //shrink the sliding window until we are left with K Distinct chars
                 char leftChar = s.charAt(windowStart);
                 //here, we are reducing the character going out the window
                 map.put(leftChar, map.get(leftChar) - 1);
                 //if after reducing the freq of the char going out of the window, and we are left with 0
                 //we then remove it from the map
-                if (map.get(leftChar) == 0){
+                if (map.get(leftChar) == 0) {
                     map.remove(leftChar);
                 }
                 //then we add the next charcater of the next window
                 windowStart++;
             }
             //keep track of the max Length so far
-            maxLength  = Math.max(maxLength, windowEnd - windowStart + 1);
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
         }
         return maxLength;
     }
